117578a108a9ec36d34fcb8d0b33db9db8224a42
daft2 <daftdevs@gmail.com>
2022-05-19 22:37:22 +0700

    ref: add title and links

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,28 @@ import RainbowHeadbar from '../components/molecules/rainbow-headbar/RainbowHeadb
 function Home() {
 	return (
 		<div>
-			<RainbowHeadbar />
+			<RainbowHeadbar title={'Pelangi'}>
+				<a
+					href="https://twitter.com/0xobject"
+					target='_blank'
+					rel="noreferrer"
+				>
+					<span className='text-lg font-extrabold text-slate-400 hover:text-blue-500'>🐣 twitter</span>
+				</a>
+				<a
+					href="https://github.com/daft2"
+					target='_blank'
+					rel="noreferrer"
+				>
+					<span className='text-lg font-extrabold text-slate-400 hover:text-black'>👨‍💻 github</span>
+				</a>
+			</RainbowHeadbar>
+			<div className="flex py-16 justify-center w-screen">
+				<h1 className='text-6xl w-6/12 text-center font-extrabold'>Explore the new world of Ethereum 🌍 🖼 ✨</h1>
+			</div>
+			<div className="flex justify-center">
+				<h1 className='text-slate-400 snap-center font-extrabold'>made with 💖 by @0xobject</h1>
+			</div>
 		</div>
 	)
 }
