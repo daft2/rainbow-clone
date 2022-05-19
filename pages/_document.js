efd99850f310a8078a4a0a0d8c761923c7a69e98
daft2 <daftdevs@gmail.com>
2022-05-19 11:20:35 +0700

    init

---
+++ pages/_document.js
@@ -0,0 +1,25 @@
+/* eslint-disable react/react-in-jsx-scope */
+// pages/_document.js
+
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+	render() {
+		return (
+			<Html>
+				<Head>
+					<link rel="preconnect" href="https://fonts.googleapis.com" />
+					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+					<link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@400;500;700&family=Pacifico&display=swap" rel="stylesheet" />
+					<script src="https://kit.fontawesome.com/e015ee243a.js" crossOrigin="anonymous"></script>
+				</Head>
+				<body className='font-poppins'>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		)
+	}
+}
+
+export default MyDocument
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+	],
 
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+	theme: {
+		extend: {
+			fontFamily: {
+				'pacifico': ['"Pacifico"', 'cursive'],
+				'm-plus': ['"M PLUS Rounded 1c"', 'sans-serif']
+			}
+		},
+	},
+	plugins: [],
+}
