d8c195de0f9e822336f350ae07df5b77164ded2b
daft2 <daftdevs@gmail.com>
2022-05-19 22:36:24 +0700

    ref: add children and title prop for customization

--- components/molecules/rainbow-headbar/RainbowHeadbar.js
+++ components/molecules/rainbow-headbar/RainbowHeadbar.js
@@ -1,15 +1,25 @@
-import GradientButton from '../../atoms/gradient-button/GradientButton'
 import InputSearch from '../../atoms/input-search/InputSearch'
 import RainbowIcon from '../../atoms/rainbow-icon/RainbowIcon'
+import PropTypes from 'prop-types'
 
-const RainbowHeadbar = () => {
+const RainbowHeadbar = ({children, title}) => {
 	return (
-		<div className='flex justify-between mt-6 mx-10'>
-			<RainbowIcon />
+		<div className='flex justify-between items-center mt-6 mx-10'>
+			<div className="flex items-center">
+				<RainbowIcon />
+				<span className='pl-3 hidden md:block text-2xl font-extrabold'>{title}</span>
+			</div>
 			<InputSearch />
-			<GradientButton>Open in the app</GradientButton>
+			<div className='flex gap-3'>
+				{children}
+			</div>
 		</div>
 	)
+}
+
+RainbowHeadbar.propTypes = {
+	children: PropTypes.node,
+	title: PropTypes.string
 }
 
 export default RainbowHeadbar
