b2b75c2bd0b97a5db252fe377954832c8cea47b0
daft2 <daftdevs@gmail.com>
2022-05-19 12:43:15 +0700

    feat: create rainbow icon

---
+++ components/atoms/rainbow-icon/RainbowIcon.js
@@ -0,0 +1,11 @@
+
+
+const RainbowIcon = () => {
+	return (
+		<div className="hover-zoom-out bg-blue-800 rounded-lg w-10 h-10 flex justify-center items-center shadow-lg">
+			<span className='text-2xl'>🌈</span>
+		</div>
+	)
+}
+
+export default RainbowIcon
