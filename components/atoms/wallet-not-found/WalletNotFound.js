bae861e3e7135cdef9019245799151c61f51816d
daft2 <daftdevs@gmail.com>
2022-05-19 22:48:28 +0700

    feat: create wallet not found component

---
+++ components/atoms/wallet-not-found/WalletNotFound.js
@@ -0,0 +1,11 @@
+
+const WalletNotFound = () => {
+	return (
+		<div className="flex flex-col w-screen h-screen justify-center items-center">
+			<span className='text-7xl'>{'👻'}</span>
+			<h1 className='text-2xl font-extrabold'>{'Oops.. there\'s nothing here!'}</h1>
+		</div>
+	)
+}
+
+export default WalletNotFound
