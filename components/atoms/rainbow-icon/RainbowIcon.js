e8ae8577a5f724cb8b0c2cbb961eb2dc90e67dc5
daft2 <daftdevs@gmail.com>
2022-05-19 21:20:16 +0700

    ref: add route to home

--- components/atoms/rainbow-icon/RainbowIcon.js
+++ components/atoms/rainbow-icon/RainbowIcon.js
@@ -1,8 +1,19 @@
+import { useRouter } from 'next/router'
 
 
 const RainbowIcon = () => {
+	const router = useRouter()
+
+	const handleClick = (e) => {
+		e.preventDefault()
+		router.push('/')
+	}
+
 	return (
-		<div className="hover-zoom-out bg-blue-800 rounded-lg w-10 h-10 flex justify-center items-center shadow-lg">
+		<div
+			className="hover-zoom-out cursor-pointer bg-blue-800 rounded-lg w-10 h-10 flex justify-center items-center shadow-lg"
+			onClick={handleClick}
+		>
 			<span className='text-2xl'>🌈</span>
 		</div>
 	)
