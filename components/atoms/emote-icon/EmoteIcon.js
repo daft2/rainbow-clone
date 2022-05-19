9b599c97aa3b7754e7a7281adf166a498fde1f69
daft2 <daftdevs@gmail.com>
2022-05-19 15:39:55 +0700

    feat: create emote icon

---
+++ components/atoms/emote-icon/EmoteIcon.js
@@ -0,0 +1,9 @@
+const EmoteIcon = () => {
+	return (
+		<div className="bg-cyan-300 shadow-lg w-fit h-fit flex justify-center items-center rounded-full p-4">
+			<span className='text-5xl'>{'🥶'}</span>
+		</div>
+	)
+}
+
+export default EmoteIcon
