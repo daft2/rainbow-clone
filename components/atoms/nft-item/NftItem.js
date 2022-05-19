7df92518d4bc34f2fcd65d72e96bf03d11223ed2
daft2 <daftdevs@gmail.com>
2022-05-19 21:17:15 +0700

    ref: show the nft metadata mainly image and title

--- components/atoms/nft-item/NftItem.js
+++ components/atoms/nft-item/NftItem.js
@@ -1,19 +1,26 @@
-import React from 'react'
 import PropTypes from 'prop-types'
+import ImageTranslate from '../../../utils/image-translate'
 
-const NftItem = (nft) => {
+const NftItem = ({ nft }) => {
+	const {title, metadata} = nft
+	let imageUrl = metadata.image || metadata.image_url
+
+	if (imageUrl?.includes('ipfs://')) imageUrl = ImageTranslate.ipfs(imageUrl)
+
 	return (
-		<div>
+		<div className='h-fit overflow-x-clip'>
 			<img
 				className='w-64 h-60 p-1 rounded-3xl drop-shadow-lg duration-300 ease-out hover:scale-105 cursor-pointer'
-				src="https://lh3.googleusercontent.com/OJDijFFOEf6UfOX1FYHvzKp6pSbHE6ExkmaINVb4Ftslr3oxhF5KEgtdc2_q4NOIf6tNQQYv7s8SNZ6sGFwm5YctmmwwIXy8kuKM=w1000"
-				alt=""
+				src={imageUrl}
+				alt="NFT Asset Image"
 			/>
-			<h2 className='text-slate-500 font-bold mt-2'>#506</h2>
+			<h2 className='text-slate-500 font-bold mt-2 truncate'>{title}</h2>
 		</div>
 	)
 }
 
-NftItem.propTypes = {}
+NftItem.propTypes = {
+	nft: PropTypes.object
+}
 
 export default NftItem
