7fd1c481cb31b637b78c115ac04aeebbc120a420
daft2 <daftdevs@gmail.com>
2022-05-21 23:06:47 +0700

    feat: loading placeholder component

---
+++ components/molecules/nft-item-loading-placeholder/NftItemLoadingPlaceholder.js
@@ -0,0 +1,11 @@
+
+const NftItemLoadingPlaceholder = () => {
+	return (
+		<div className="flex animate-pulse">
+			<div className="w-52 h-52 bg-slate-400 rounded-3xl mx-2"></div>
+			<div className="w-52 h-52 bg-slate-400 rounded-3xl mx-2"></div>
+		</div>
+	)
+}
+
+export default NftItemLoadingPlaceholder
---
+++ components/molecules/wallet-info-loading-placeholder/WalletInfoLoadingPlaceholder.js
@@ -0,0 +1,11 @@
+
+const WalletInfoLoadingPlaceholder = () => {
+	return (
+		<div className="animate-pulse">
+			<div className="h-8 my-2 w-3/4 bg-slate-400 rounded"></div>
+			<div className="h-8 w-3/5 bg-slate-400 rounded"></div>
+		</div>
+	)
+}
+
+export default WalletInfoLoadingPlaceholder
