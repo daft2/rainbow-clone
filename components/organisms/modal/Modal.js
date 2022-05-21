aceabdabb544ddd239c16a8f23c0c5cb04c50129
daft2 <daftdevs@gmail.com>
2022-05-21 14:29:56 +0700

    ref: show selected nft data on modal

--- components/organisms/modal/Modal.js
+++ components/organisms/modal/Modal.js
@@ -1,8 +1,12 @@
 import {useState, useEffect} from 'react'
 import PropTypes from 'prop-types'
+import ImageTranslate from '../../../utils/image-translate'
 
-const Modal = ({isOpen}) => {
+const Modal = ({ isOpen, selectedNft}) => {
 	const [isVisible, setIsVisible] = useState(isOpen)
+	let imageUrl = selectedNft?.metadata.image || selectedNft?.metadata.image_url
+
+	if (imageUrl?.includes('ipfs://')) imageUrl = ImageTranslate.ipfs(imageUrl)
 
 	const handleClose = () => {
 		setIsVisible(false)
@@ -11,7 +15,6 @@ const Modal = ({isOpen}) => {
 	useEffect(() => {
 		setIsVisible(isOpen)
 	}, [isOpen])
-
 
 	return (
 		<div className={`${!isVisible && 'hidden'}`}>
@@ -27,22 +30,28 @@ const Modal = ({isOpen}) => {
 					className="relative flex justify-center sm:items-center sm:flex-col md:items-start md:flex-row">
 					<img
 						className='rounded-3xl w-96 mb-5 shadow-2xl'
-						src="https://lh3.googleusercontent.com/4t7b9pgWwoIk8WgFzs-alBR8uy9DIOdQ8cIl3-3MYHF9uvDI3ihBM4vU1TRsjOvgbd7AZGJDDnsIPuhxqyQdqZKQ_RpIxaiVInhC=w500"
+						src={imageUrl}
 						alt=""
 					/>
 					<div
 						className="bg-white h-fit mx-5 p-5 w-96 flex flex-wrap flex-col shadow-2xl rounded-3xl">
 						<h2 className='font-bold text-black text-opacity-50'>Collection Name</h2>
-						<h1 className='font-extrabold text-2xl mb-3'>Name</h1>
+						<h1 className='font-extrabold text-2xl mb-3'>{selectedNft?.title}</h1>
 						<div>
 							<h2 className='font-extrabold text-lg my-2'>Description</h2>
-							<p className='text-black text-opacity-50 text-md'>598</p>
+							<p className='text-black text-opacity-50 text-md'>{selectedNft?.description}</p>
 						</div>
 						<div>
 							<h2 className='font-extrabold text-lg my-2'>Attributes</h2>
-							<div className="flex flex-col text-black text-opacity-50 bg-gray-100 rounded-3xl w-fit py-2 pl-2 pr-4">
-								<h3 className='font-extrabold text-xs uppercase'>Accessory</h3>
-								<h3 className='text-sm'>Eye Mask</h3>
+							<div className="flex flex-wrap">
+								{selectedNft?.metadata.attributes && selectedNft?.metadata.attributes.map((attribute,index) =>
+									<div
+										key={`attribute-${attribute.trait_type}-${index}`}
+										className="flex flex-col mr-1 mt-1 text-black text-opacity-50 bg-gray-100 rounded-3xl w-fit py-2 pl-2 pr-4">
+										<h3 className='font-extrabold text-xs uppercase'>{attribute.trait_type}</h3>
+										<h3 className='text-sm'>{attribute.value}</h3>
+									</div>
+								)}
 							</div>
 						</div>
 					</div>
@@ -53,7 +62,8 @@ const Modal = ({isOpen}) => {
 }
 
 Modal.propTypes = {
-	isOpen: PropTypes.bool
+	isOpen: PropTypes.bool,
+	selectedNft: PropTypes.object
 }
 
 export default Modal
