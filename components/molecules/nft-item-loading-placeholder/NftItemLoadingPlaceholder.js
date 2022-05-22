265a870a75d5e40675b5ea5351371ddb9d45bb94
daft2 <daftdevs@gmail.com>
2022-05-22 22:27:40 +0700

    ref: add size for every screen size to match

--- components/molecules/nft-item-loading-placeholder/NftItemLoadingPlaceholder.js
+++ components/molecules/nft-item-loading-placeholder/NftItemLoadingPlaceholder.js
@@ -2,8 +2,8 @@
 const NftItemLoadingPlaceholder = () => {
 	return (
 		<div className="flex animate-pulse">
-			<div className="w-52 h-52 bg-slate-400 rounded-3xl mx-2"></div>
-			<div className="w-52 h-52 bg-slate-400 rounded-3xl mx-2"></div>
+			<div className="w-32 h-32 sm:w-36 sm:h-36 md:w-44 md:h-44 xl:w-56 xl:h-56 bg-slate-400 rounded-3xl mx-2"></div>
+			<div className="w-32 h-32 sm:w-36 sm:h-36 md:w-44 md:h-44 xl:w-56 xl:h-56 bg-slate-400 rounded-3xl mx-2"></div>
 		</div>
 	)
 }
