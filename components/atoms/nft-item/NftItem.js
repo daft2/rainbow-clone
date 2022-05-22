f105a7239e2587640f635ca48171f7ba062afcc7
daft2 <daftdevs@gmail.com>
2022-05-22 22:00:25 +0700

    ref: add various fixed image size across device

--- components/atoms/nft-item/NftItem.js
+++ components/atoms/nft-item/NftItem.js
@@ -12,7 +12,7 @@ const NftItem = ({ nft, handleModal }) => {
 		<div className={`${!title ? 'hidden' : null} w-fit h-fit`}>
 			<div className="bg-theme shadow-around rounded-3xl w-32 sm:w-36 md:w-44 xl:w-56">
 				<img
-					className='object-cover rounded-3xl w-fit h-fit duration-300 ease-out hover:scale-105 cursor-pointer'
+					className='object-cover rounded-3xl w-32 h-32 sm:w-36 sm:h-36 md:w-44 md:h-44 xl:w-56 xl:h-56 duration-300 ease-out hover:scale-105 cursor-pointer'
 					src={imageUrl}
 					alt="NFT Asset Image"
 					onClick={handleModal}
