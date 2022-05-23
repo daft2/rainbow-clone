5b51767b0e0413ebe12c702b628ade7a209d3e5a
daft2 <daftdevs@gmail.com>
2022-05-23 14:31:33 +0700

    ref: show svg NFT and add video nft asset support

--- components/atoms/nft-item/NftItem.js
+++ components/atoms/nft-item/NftItem.js
@@ -1,24 +1,37 @@
 import PropTypes from 'prop-types'
 import ImageTranslate from '../../../utils/image-translate'
+import RegexTranslate from '../../../utils/regex-translate'
 
 const NftItem = ({ nft, handleModal }) => {
-	const {title, metadata} = nft
-	let imageUrl = metadata.image || metadata.image_url
+	const { title, metadata, tokenUri } = nft
+	const uriMetadata = RegexTranslate.getMetadataFromURI(tokenUri.raw)
+
+	let imageUrl = metadata.image || metadata.image_url || uriMetadata?.image
+	let nftName = title || uriMetadata?.name
 
 	if (imageUrl?.includes('ipfs://')) imageUrl = ImageTranslate.ipfs(imageUrl)
 
 	return (
-		// TODO: Temporary fixes. Need to change later
-		<div className={`${!title ? 'hidden' : null} h-fit`}>
+		<div className={'h-fit'}>
 			<div className="bg-theme shadow-around rounded-3xl w-32 sm:w-36 md:w-44 xl:w-56 h-fit">
-				<img
-					className='object-cover rounded-3xl w-32 h-32 sm:w-36 sm:h-36 md:w-44 md:h-44 xl:w-56 xl:h-56 duration-300 ease-out hover:scale-105 cursor-pointer'
-					src={imageUrl}
-					alt="NFT Asset Image"
-					onClick={handleModal}
-				/>
+				{imageUrl?.includes('.mp4') ?
+					<video
+						className='object-cover rounded-3xl w-32 h-32 sm:w-36 sm:h-36 md:w-44 md:h-44 xl:w-56 xl:h-56 duration-300 ease-out hover:scale-105 cursor-pointer'
+						src={imageUrl}
+						alt="NFT Asset Image"
+						preload='auto'
+						autoPlay
+						onClick={handleModal}
+					/>
+					: <img
+						className='object-cover rounded-3xl w-32 h-32 sm:w-36 sm:h-36 md:w-44 md:h-44 xl:w-56 xl:h-56 duration-300 ease-out hover:scale-105 cursor-pointer'
+						src={imageUrl}
+						alt="NFT Asset Image"
+						onClick={handleModal}
+					/>
+				}
 			</div>
-			<h2 className='text-slate-500 font-bold mt-2 truncate'>{title}</h2>
+			<h2 className='text-slate-500 font-bold mt-2 truncate'>{nftName}</h2>
 		</div>
 	)
 }
